1bf6d71320056d1d49c66b0451421543d26b4d63
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-06-04 11:34:22 -0400

    Added search when "return" key pressed

--- aliens/js/app.js
+++ aliens/js/app.js
@@ -6,6 +6,17 @@ var $searchBtn = document.querySelector("#searchBtn");
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
+// Execute a function when the user releases a key on the keyboard
+$searchInput.addEventListener("keydown", function(event) {
+  // Cancel the default action, if needed
+  // event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    $searchBtn.click();
+  }
+});
+
 // Set filteredAddresses to addressData initially
 var data = dataSet;
 
